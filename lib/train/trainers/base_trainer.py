bdd93e7b9a7693c48e605f4a8f847fe4f4156700
Yifan Bai <826980835@qq.com>
2024-10-23 12:22:53 +0800

    Update base_trainer.py

--- lib/train/trainers/base_trainer.py
+++ lib/train/trainers/base_trainer.py
@@ -92,7 +92,7 @@ class BaseTrainer:
                     # only save the last 10 checkpoints
                     save_every_epoch = getattr(self.settings, "save_every_epoch", False)
                     save_epochs = []
-                    if epoch > (max_epochs - 1) or save_every_epoch or epoch % 40 == 0 or epoch in save_epochs or epoch > (max_epochs - 5):
+                    if epoch > (max_epochs - 1) or save_every_epoch or epoch % 5 == 0 or epoch in save_epochs or epoch > (max_epochs - 5):
                     # if epoch > (max_epochs - 10) or save_every_epoch or epoch % 100 == 0:
                         if self._checkpoint_dir:
                             if self.settings.local_rank in [-1, 0]:
